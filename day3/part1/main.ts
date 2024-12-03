c529ccd7a7127f42e78947168b093accf80acb06
Anmol Vijaywargiya <anmol.vijaywargiya@whitelabel-loyalty.com>
2024-12-03 23:18:45 +0530

    Finish implementation of day3 part1

--- day3/part1/main.ts
+++ day3/part1/main.ts
@@ -1,3 +1,23 @@
+import { parseFile } from './parser';
+
 export const calculate = (instruction: string) => {
-    return 0;
-}
+  const matches = instruction.matchAll(/mul\(\d+,\d+\)/g);
+  let sum = 0;
+  for (const match of matches) {
+    const numbers = match[0].matchAll(/\d+/g);
+    let mul = 1;
+    for (const num of numbers) {
+      mul = mul * parseInt(num[0]);
+    }
+    sum += mul;
+  }
+  return sum;
+};
+
+console.log(
+  calculate(
+    await parseFile(
+      '/Users/1.mole/Documents/repositories/personal/advent-of-code-2024/day3/part1/input.txt'
+    )
+  )
+);
