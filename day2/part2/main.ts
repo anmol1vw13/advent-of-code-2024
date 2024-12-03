e74e1d5094ef6f204005fadd24e9757c11fd74c7
Anmol Vijaywargiya <anmol.vijaywargiya@whitelabel-loyalty.com>
2024-12-03 21:41:58 +0530

    Solved Day2 Part2

---
+++ day2/part2/main.ts
@@ -0,0 +1,60 @@
+import { parseFile } from './parser';
+
+export const checkSafe = (arr: number[]) => {
+  if (_checkSafe(arr)) {
+    return true;
+  }
+  let i = -0;
+
+  while (i <= arr.length) {
+    const updatedArr = arr.filter((_, index) => index != i);
+    if (_checkSafe(updatedArr)) {
+      return true;
+    }
+    i++;
+  }
+  return false;
+};
+
+export const _checkSafe = (arr: number[]) => {
+  let currentFlow: 'ASC' | 'DESC';
+  let safe = true;
+  arr.some((v, index) => {
+    if (index + 1 === arr.length) {
+      safe = true;
+      return true;
+    }
+
+    const diff = Math.abs(v - arr[index + 1]);
+    if (diff < 1 || diff > 3) {
+      safe = false;
+      return true;
+    }
+
+    if (currentFlow === 'ASC' && v > arr[index + 1]) {
+      safe = false;
+      return true;
+    } else if (currentFlow === 'DESC' && v < arr[index + 1]) {
+      safe = false;
+      return true;
+    }
+
+    if (v < arr[index + 1]) {
+      currentFlow = 'ASC';
+    } else if (v > arr[index + 1]) {
+      currentFlow = 'DESC';
+    }
+  });
+  return safe;
+};
+
+const run = async (filePath: string) => {
+  const twoDArray = await parseFile(filePath);
+  return twoDArray.filter((arr) => checkSafe(arr)).length;
+};
+
+console.log(
+  await run(
+    '/Users/1.mole/Documents/repositories/personal/advent-of-code-2024/day2/part1/input.txt'
+  )
+);
